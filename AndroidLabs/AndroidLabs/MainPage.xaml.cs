a33ae58978d6d52e288fab1556b2e86add272eb6
RiabovAndrew <43186510+RiabovAndrew@users.noreply.github.com>
2021-10-31 17:20:07 +0200

    initial commit

---
+++ AndroidLabs/AndroidLabs/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Animation;
+using Xamarin.Forms;
+
+namespace AndroidLabs
+{
+    public partial class MainPage : ContentPage
+    {
+        private int num;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void ButtonClicked(object sender, EventArgs e)
+        {
+            //label1.Text = (++num).ToString();
+            await label1.RotateTo(360, 2000);
+        }
+
+        private async void ButtonClicked2(object sender, EventArgs e)
+        {
+            await label1.RotateTo(360, 2000);
+        }
+    }
+}
